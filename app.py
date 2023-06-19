538eb9a8e1e7d2d09cbdb5ff62e8ec9320c7b568
Oscar Castillo <oscar00129@gmail.com>
2023-06-19 07:37:18 -0600

    Change Tokenizer to Nagisa module

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 from janome.tokenizer import Tokenizer
-import json
+import json, nagisa
 from routes import auth_routes
 
 app = Flask(__name__)
@@ -41,6 +41,27 @@ def process():
             i += 1
 
     print(words)
+
+    return json.dumps({ 'data': { 'words': words } })
+
+@app.route('/nagisa', methods=['POST'])
+def nagisa_method():
+    data = request.get_json()['data']
+    text = data['text']
+    words = nagisa.tagging(text).words
+
+    # Utilizamos la función map() para reemplazar " \n" por "\n" en cada palabra
+    words = list(map(lambda word: "\n" if word == " \n" or word == "\u3000" else word, words))
+
+    # Recorremos la lista y eliminamos un elemento si hay dos "\n" seguidos
+    i = 0
+    while i < len(words) - 1:
+        if words[i] == "\n" and words[i+1] == "\n":
+            words.pop(i)
+        elif words[i] == " ":
+            words.pop(i)
+        else:
+            i += 1
 
     return json.dumps({ 'data': { 'words': words } })
 
