43a4aac5428a810db8ff0d87b9af0c77c168e298
Oscar Castillo <oscar00129@gmail.com>
2023-06-26 11:51:44 -0600

    Add text page

--- routes/text_routes.py
+++ routes/text_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, session, render_template, redirect, url_for
+from flask import Blueprint, session, render_template, redirect, url_for, request
 import helpers, mysql.connector, json
 
 text_bp = Blueprint('text', __name__)
@@ -94,3 +94,55 @@ def get_text(text_id):
         return render_template('text.html', data = data)
     else:
         return redirect(url_for('login'))
+
+@text_bp.route('/texts/add')
+def add_text():
+    logged_user = session.get('logged_user')
+    if logged_user:
+        # Get language data
+        language = helpers.readConfig('APP', 'LANGUAGE')
+        data = helpers.getLanguageData(language)
+
+        # Put logged user info
+        data['logged_user'] = logged_user
+
+        return render_template('add.html', data = data)
+    else:
+        return redirect(url_for('login'))
+
+@text_bp.route('/texts/addTextBackend', methods=['POST'])
+def add_text_backend():
+    language = helpers.readConfig('APP', 'LANGUAGE')
+    data = helpers.getLanguageData(language)
+
+    title = request.get_json()['title']
+    img_src = request.get_json()['img_src']
+    stats = request.get_json()['stats']
+    text = request.get_json()['text']
+    author_id = request.get_json()['author_id']
+    is_public = request.get_json()['is_public']
+
+    cn = mysql.connector.connect(
+        host = helpers.readConfig('DATABASE', 'HOST'),
+        port = helpers.readConfig('DATABASE', 'PORT', True),
+        user = helpers.readConfig('DATABASE', 'USER'),
+        password = helpers.readConfig('DATABASE', 'PASSWORD'),
+        database = helpers.readConfig('DATABASE', 'DATABASE')
+    )
+    cursor = cn.cursor()
+    
+    # Registro
+    query = "INSERT INTO texts (title, img_src, stats, text, author_id, is_public) VALUES (%s, %s, %s, %s, %s, %s);"
+    params = (title, img_src, stats, text, author_id, is_public)
+    cursor.execute(query, params)
+    cn.commit()
+    if cursor.rowcount > 0:
+        cursor.close()
+        cn.close()
+        # El registro fue exitoso
+        return json.dumps({ 'success': data['language_data']['text_data']['messages']['success'] })
+    else:
+        cursor.close()
+        cn.close()
+        # El registro falló
+        return json.dumps({ 'error': data['language_data']['text_data']['messages']['error'] })
